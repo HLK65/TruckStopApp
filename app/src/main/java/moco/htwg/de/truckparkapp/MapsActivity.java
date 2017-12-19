336b4d7e592e0b034f7941e6559ecb070730a6c2
Sebastian Thümmel <sethuemm@htwg-konstanz.de>
2017-12-19 14:03:22 +0100

    implemented position tracking and camera movement to current position. right now the update interval is about 5 seconds

--- app/src/main/java/moco/htwg/de/truckparkapp/MapsActivity.java
+++ app/src/main/java/moco/htwg/de/truckparkapp/MapsActivity.java
@@ -1,27 +1,78 @@
 package moco.htwg.de.truckparkapp;
 
+
+
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+
+    private GoogleMap map;
+    private boolean locationPermissionGranted;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location lastKnownPosition;
+    public static int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    public static float DEFAULT_ZOOM = 17.0f;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private LocationSettingsRequest locationSettingsRequest;
+    private SettingsClient settingsClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingRequest();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
     }
 
 
@@ -36,11 +87,131 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map = googleMap;
+        updateLocationUI();
+        getDeviceLocation();
+        startLocationUpdates();
     }
+
+    private void updateLocationUI() {
+        if (map == null) {
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            getLocationPermission();
+            //locationPermissionGranted = true;
+        }
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    private void getDeviceLocation(){
+        try {
+            if(locationPermissionGranted) {
+                getLocationPermission();
+            }
+            Task locationResult = fusedLocationProviderClient.getLastLocation();
+            locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    if(task.isSuccessful() && task.getResult() != null){
+                        lastKnownPosition = task.getResult();
+                        System.out.println("lastKnownPosition: "+ lastKnownPosition);
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastKnownPosition.getLatitude(), lastKnownPosition.getLongitude()), DEFAULT_ZOOM));
+
+
+                    }
+                }
+            });
+
+        } catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation", e);
+        }
+    }
+
+    private void buildLocationSettingRequest(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+    }
+
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        /**
+         * desired request interval. may be faster or lower, depends on other applications (faster) and location sources (lower)
+         */
+        locationRequest.setInterval(5000);
+        /**
+         * update wont be faster than entered interval
+         */
+        locationRequest.setFastestInterval(5000);
+        /**
+         * self explaining
+         */
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void createLocationCallback(){
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                super.onLocationResult(locationResult);
+                Location location = locationResult.getLastLocation();
+                System.out.println("location: "+ location);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), DEFAULT_ZOOM));
+            }
+        };
+    }
+
+    private void startLocationUpdates(){
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+            .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                @SuppressLint("MissingPermission")
+                @Override
+                public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    Log.i(TAG, "location settings successful checked");
+                    getLocationPermission();
+                    fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+                }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch(statusCode) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                            resolvableApiException.startResolutionForResult(MapsActivity.this, 0x1);
+                            Log.i(TAG, "resolved resolution required error");
+                        } catch (IntentSender.SendIntentException e1) {
+                            Log.i(TAG, "unable to resolve error");
+                        }
+                        break;
+                }
+                Log.e(TAG, "location settings error", e);
+            }
+        });
+    }
+
+
+
+    private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
 }
